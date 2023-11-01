4e7c756a6b21a9a2f85fd436e196b3cae881130a
jeremycohn <jeremycohn@localhost>
2023-11-01 15:52:36 -0500

    done

--- src/studio7/Fraction.java
+++ src/studio7/Fraction.java
@@ -2,4 +2,14 @@ package studio7;
 
 public class Fraction {
 
+	private int num;
+	private int den;
+	public Fraction(int n,int d) {
+		this.num = n;
+		this.den = d;
+	}
+	public Fraction addFrac(Fraction f) {
+		f = this.num / this.den;
+		return f;
+	}
 }
